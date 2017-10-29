e65dc1e95badd43f2ca56222f03eb9d6b4312da7
Facundo Rossi <frossi85@gmail.com>
2017-10-29 02:48:38 -0300

    Model working, some docs improvements and images from the different tests

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 # prob-inference-engine-tp2
 
-https://www.bayesserver.com/docs/introduction/bayesian-networks
-https://www.norsys.com/tutorials/netica/secA/tut_A1.htm
+## Uso
+
+Para poder correr el modelo es necesario tener instalado ProbLog:
+
+Python 2: ```sudo pip install problog```
+Python 3: ```sudo pip3 install problog```
+
+El modelo se puede ejecutar con el comando
+
+```problog model.pl```
+
+
+## Modelo
+
+## Referencias 
+* https://www.bayesserver.com/docs/introduction/bayesian-networks
+* https://www.norsys.com/tutorials/netica/secA/tut_A1.htm
+* https://github.com/eBay/bayesian-belief-networks/blob/master/bayesian/examples/bif/asia.bif
--- model.pl
+++ model.pl
@@ -3,70 +3,68 @@
 
 
 %Has Tuberculosis
-0.0104::hasTuberculosis :- visitToAsia.
-0.9896::hasTuberculosis :- \+visitToAsia.
+0.05::hasTuberculosis :- visitToAsia.
+0.01::hasTuberculosis :- \+visitToAsia.
 
 
 %Smoker
-0.5::smoker.
+0.50::smoker.
 
 
 %Has Lung Cancer
-0.055::hasLungCancer :- smoker.
-0.9450::hasLungCancer :- \+smoker.
-
-
-
+0.10::hasLungCancer :- smoker.
+0.01::hasLungCancer :- \+smoker.
 
 
 %Has Bronchitis
-0.45::hBCause1.
-0.55::hBCause2.
-
-hasBronchitis :- smoker, hBCause1.
-hasBronchitis :- \+smoker, hBCause2.
+0.60::hasBronchitis :- smoker.
+0.30::hasBronchitis :- \+smoker.
 
 
 %TuberculosisOrCancer
-
-0.000572::tOCCause1.
-0.054428::tOCCause2.
-0.009828::tOCCause3.
-0.935172::tOCCause4.
-
-tuberculosisOrCancer :- hasLungCancer, hasTuberculosis, tOCCause1.
-tuberculosisOrCancer :- hasLungCancer, \+hasTuberculosis, tOCCause2.
-tuberculosisOrCancer :- \+hasLungCancer, hasTuberculosis, tOCCause3.
-tuberculosisOrCancer :- \+hasLungCancer, \+hasTuberculosis, tOCCause4.
+1.00::eitherTuberculosisOrCancer :- hasLungCancer, hasTuberculosis.
+1.00::eitherTuberculosisOrCancer :- hasLungCancer, \+hasTuberculosis.
+1.00::eitherTuberculosisOrCancer :- \+hasLungCancer, hasTuberculosis.
+0.00::eitherTuberculosisOrCancer :- \+hasLungCancer, \+hasTuberculosis.
 
 
 %XRay Result
-0.1103::xRRCause1.
-0.8897::xRRCause2.
-
-xRayResult :- tuberculosisOrCancer, xRRCause1.
-xRayResult :- \+tuberculosisOrCancer, xRRCause2.
+0.98::positiveXRayResult :- eitherTuberculosisOrCancer.
+0.05::positiveXRayResult :- \+eitherTuberculosisOrCancer.
 
 
 %Dyspnea
-
-0.02916::dCause1.
-0.03564::dCause2.
-0.42084::dCause3.
-0.51436::dCause4.
-
-dyspnea :- tuberculosisOrCancer, hasBronchitis, dCause1.
-dyspnea :- tuberculosisOrCancer, \+hasBronchitis, dCause2.
-dyspnea :- \+tuberculosisOrCancer, hasBronchitis, dCause3.
-dyspnea :- \+tuberculosisOrCancer, \+hasBronchitis, dCause4.
+0.90::dyspnea :- eitherTuberculosisOrCancer, hasBronchitis.
+0.70::dyspnea :- eitherTuberculosisOrCancer, \+hasBronchitis.
+0.80::dyspnea :- \+eitherTuberculosisOrCancer, hasBronchitis.
+0.10::dyspnea :- \+eitherTuberculosisOrCancer, \+hasBronchitis.
 
 
-evidence(visitToAsia,true).
-evidence(smoker,false).
-evidence(hasBronchitis,false).
-evidence(dyspnea,true).
+%Test case 1 - without any evidence
 
-query(xRayResult).
+%Test case 2
+%evidence(visitToAsia,true).
+
+%Test case 3
+%evidence(visitToAsia,true).
+%evidence(smoker,true).
+
+%Test case 4
+%evidence(visitToAsia,true).
+%evidence(smoker,true).
+%evidence(positiveXRayResult,true).
+
+%Test case 5
+%evidence(visitToAsia,true).
+%evidence(smoker,true).
+%evidence(positiveXRayResult,true).
+%evidence(dyspnea,true).
+
+query(visitToAsia).
 query(hasTuberculosis).
-query(tuberculosisOrCancer).
+query(smoker).
 query(hasLungCancer).
+query(eitherTuberculosisOrCancer).
+query(positiveXRayResult).
+query(hasBronchitis).
+query(dyspnea).
